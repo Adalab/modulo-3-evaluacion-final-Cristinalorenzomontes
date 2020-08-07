26c8ce6479d20d3feb376ff8af7b9dedcc779668
Christina <christinalorenzomontes@gmail.com>
2020-08-07 08:16:10 +0200

    Add README

---
+++ src/components/pricing.js
@@ -0,0 +1,21 @@
+import React from "react";
+class Pricing extends React.Component {
+  render() {
+    return (
+      <p>
+        Quisque et nisl vitae arcu fermentum semper a eget nisl. Fusce sed magna
+        nunc. Etiam id volutpat arcu. Phasellus scelerisque elit ex, sed laoreet
+        ligula volutpat eu. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Ut tincidunt ultricies dolor, mollis pretium risus viverra eget.
+        Maecenas hendrerit vestibulum velit, et suscipit est mollis iaculis.
+        Fusce tincidunt purus ac velit placerat luctus. Nunc tempor nunc vitae
+        dui pellentesque laoreet. Aenean fermentum ex nec felis congue
+        sollicitudin. Quisque posuere felis a elit viverra, egestas laoreet
+        lectus malesuada. Proin ac pulvinar dolor, in mattis risus. Proin ut mi
+        non arcu condimentum iaculis.
+      </p>
+    );
+  }
+}
+
+export default Pricing;
