826619083c2864b1bb41a1c1c7d68eb2cbab0521
Christina <christinalorenzomontes@gmail.com>
2020-08-10 10:35:38 +0200

    Change border-radius

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,14 +6,13 @@ import PropTypes from "prop-types";
 const Character = (props) => {
   return (
     <article className="card">
-      <img src={props.image} className="card__img" alt={props.name} />
-      <div className="atributes">
-        <h3 className="card__title">{props.name}</h3>
-        <p className="card__species">{props.species}</p>
-        <p className="card__status">{props.status}</p>
-      </div>
       <Link to={`/character/${props.id}`}>
-        <button className="card__btn">Click here to see in detail</button>
+        <img src={props.image} className="card__img" alt={props.name} />
+        <div className="atributes">
+          <h3 className="card__title">{props.name}</h3>
+          <p className="card__species">{props.species}</p>
+          <p className="card__status">{props.status}</p>
+        </div>
       </Link>
     </article>
   );
